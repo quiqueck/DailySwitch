98aef5bb64706fc54c8e0ed1e29d8e5f2715ec15
Frank Bauer <developer@ambertation.de>
2019-03-27 20:20:12 +0100

    removed returns form NopSerial

--- src/NopSerial.h
+++ src/NopSerial.h
@@ -9,32 +9,32 @@ class NopSerial {
     public:
     NopSerial(uint16_t xx){  }
     inline void begin(unsigned long baud, uint32_t config=SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, bool invert=false, unsigned long timeout_ms = 20000UL) const {}
-    inline size_t printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3))) { return 0; }
-    inline size_t print(const __FlashStringHelper *) const { return 0; }
-    inline size_t print(const String &) const { return 0; }
-    inline size_t print(const char[]) const { return 0; }
-    inline size_t print(char) const { return 0; }
-    inline size_t print(unsigned char, int = DEC) const { return 0; }
-    inline size_t print(int, int = DEC) const { return 0; }
-    inline size_t print(unsigned int, int = DEC) const { return 0; }
-    inline size_t print(long, int = DEC) const { return 0; }
-    inline size_t print(unsigned long, int = DEC) const { return 0; }
-    inline size_t print(double, int = 2) const { return 0; }
-    inline size_t print(const Printable&) const { return 0; }
-    inline size_t print(struct tm * timeinfo, const char * format = NULL) const { return 0; }
-    inline size_t println(const __FlashStringHelper *) const { return 0; }
-    inline size_t println(const String &s) const { return 0; }
-    inline size_t println(const char[]) const { return 0; }
-    inline size_t println(char) const { return 0; }
-    inline size_t println(unsigned char, int = DEC) const { return 0; }
-    inline size_t println(int, int = DEC) const { return 0; }
-    inline size_t println(unsigned int, int = DEC) const { return 0; }
-    inline size_t println(long, int = DEC) const { return 0; }
-    inline size_t println(unsigned long, int = DEC) const { return 0; }
-    inline size_t println(double, int = 2) const { return 0; }
-    inline size_t println(const Printable&) const { return 0; }
-    inline size_t println(struct tm * timeinfo, const char * format = NULL) const { return 0; }
-    inline size_t println(void) const { return 0; }
+    inline void printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3))) { }
+    inline void print(const __FlashStringHelper *) const { }
+    inline void print(const String &) const { }
+    inline void print(const char[]) const { }
+    inline void print(char) const { }
+    inline void print(unsigned char, int = DEC) const { }
+    inline void print(int, int = DEC) const { }
+    inline void print(unsigned int, int = DEC) const { }
+    inline void print(long, int = DEC) const { }
+    inline void print(unsigned long, int = DEC) const { }
+    inline void print(double, int = 2) const { }
+    inline void print(const Printable&) const { }
+    inline void print(struct tm * timeinfo, const char * format = NULL) const { }
+    inline void println(const __FlashStringHelper *) const { }
+    inline void println(const String &s) const { }
+    inline void println(const char[]) const { }
+    inline void println(char) const { }
+    inline void println(unsigned char, int = DEC) const { }
+    inline void println(int, int = DEC) const { }
+    inline void println(unsigned int, int = DEC) const { }
+    inline void println(long, int = DEC) const { }
+    inline void println(unsigned long, int = DEC) const { }
+    inline void println(double, int = 2) const { }
+    inline void println(const Printable&) const { }
+    inline void println(struct tm * timeinfo, const char * format = NULL) const { }
+    inline void println(void) const { }
 };
 
 #ifdef DEBUG_LOG
