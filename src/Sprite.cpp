1fae7e2f8f40106b00748e432cacdeaad63b57f4
Frank Bauer <developer@ambertation.de>
2019-03-27 15:49:03 +0100

    fixed alphaBlend

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -38,22 +38,12 @@ void  mySprite::pushImageAlpha(int32_t x, int32_t y, uint32_t w, uint32_t h, con
           inCol.value = inCol.value<<8 | inCol.value>>8;
           color.value = color.value<<8 | color.value>>8;        
         }
-        const uint16_t rIn = (color.value >> 8) & 0x00F8;
-        const uint16_t gIn = (color.value >> 3) & 0x00FC;
-        const uint16_t bIn = (color.value << 3) & 0x00F8;
         
-        const uint16_t bSrc = (inCol.value >> 8) & 0x00F8;
-        const uint16_t gSrc = (inCol.value >> 3) & 0x00FC;
-        const uint16_t rSrc = (inCol.value << 3) & 0x00F8;    
-        const uint16_t aSrc = (0xff - color.alpha);
-
-        const uint16_t rOut = (color.alpha * rIn + aSrc * rSrc) >> 8;
-        const uint16_t gOut = (color.alpha * gIn + aSrc * gSrc) >> 8;
-        const uint16_t bOut = (color.alpha * bIn + aSrc * bSrc) >> 8;
-
-        color.value = ((rOut & 0xF8) << 8) | ((gOut & 0xFC) << 3) | (bOut >> 3);
+        color.value = alphaBlend(color.alpha, color.value, inCol.value);
+        
         if(_iswapBytes) color.value = color.value<<8 | color.value>>8;
         _img[addr] = color.value;
+        
         x++;
       }
       ys++;
